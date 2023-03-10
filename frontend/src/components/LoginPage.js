b2116a575c2c5ca17ab38c1995502eed067adba0
subway1112 <subway1112@naver.com>
2023-03-10 18:13:07 +0900

    feat: 카카오 API 로그인 기능 추가, fix: LoginPage.css 파일 수정

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,16 +1,22 @@
-import logintext from "./static/login-text.png";
-import "./stylesheets/login.css";
+import "../static/css/LoginPage.css";
+import googlebtn from "../static/images/googlebtn.png"
+import applebtn from "../static/images/applebtn.png"
+import facebookbtn from "../static/images/facebookbtn.png"
+import KakaoLogin from "./LoginApi/KakaoLogin";
 
 function LoginPage() {
   return (
     <>
+    
+    <div class="container">
+
       <p class="logintext">Login</p>
-      <div>
+      <div class="id-container">
         <p class="id-text">Email</p>
         <input class="id-input" placeholder="   Email Address" />
       </div>
-      <img src="" />
-      <div>
+
+      <div class="pw-container">
         <p class="pw-text">Password</p>
         <input class="pw-input" placeholder="   Password" />
       </div>
@@ -18,20 +24,21 @@ function LoginPage() {
         <input type="checkbox" name="onoff-switch" id="onoff-switch1" />
         <label for="onoff-switch1"></label>
         <span class="toggle-text">Remember me</span>
-        <a>forgot password?</a>
+        <a class="find-pw">forgot password?</a>
       </div>
       <div>
       <button class="login-btn">Login</button>
-    </div>
-    
-    <div class="api-btn">
-      <button></button>
-      <button></button>
-      <button></button>
-    </div>
+      </div>
+      
+      <div class="api-btn">
+        <button><img src={googlebtn}></img></button>
+        <button><img src={applebtn}></img></button>
+        <button onClick={KakaoLogin}><img src={facebookbtn}></img></button>
+      </div>
 
-    <div class="signup-div">
-      <p>Don't have an account? <span class="signup"> Sign up</span></p>
+      <div class="signup-div">
+        <p>Don't have an account? <span><a class="signup-btn"> Sign up</a></span></p>
+      </div>
     </div>
     </>
   );
