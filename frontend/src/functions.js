795d0df5bd0bf8e92ea70dcf8f7774440bab867d
jaebyeoklee <jaebyeoklee@gmail.com>
2023-03-10 18:30:03 +0900

    feat: backend 회사정보 api 추가, db 회사테이블 정리 완료

--- frontend/src/functions.js
+++ frontend/src/functions.js
@@ -1,10 +1,12 @@
 import axios from "axios";
-export let dataSet = null;
-export function callApi(url) {
-  axios
+export async function callApi(url) {
+  let dataSet = null;
+  await axios
     .get(url)
     .then((response) => {
       dataSet = response.data;
     })
     .catch((error) => console.log(error));
+
+  return dataSet;
 }
