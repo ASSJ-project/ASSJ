2bd4d6310b250b4ab67556710cc56f799e0b5215
jaebyeoklee <jaebyeoklee@gmail.com>
2023-03-13 11:35:05 +0900

    fix:비동기데이터 처리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,27 @@
 import { callApi } from "./functions";
+import { useState, useEffect } from "react";
 // import { useState } from "react";
 import React from "react";
 
 function App() {
   const url = "api/getCorpData/address=gangnam/jobsCode=1";
-  async function fetchData() {
-    const dataList = await callApi(url);
-    const addr = await dataList.map((item) => {
-      return { title: item.title, address: item.basicAddr };
-    });
-    console.log(addr);
-
-    // return addr;
-  }
-  fetchData();
+  const [addrData, setAddrData] = useState();
+  const [isError, setError] = useState(false);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const dataList = await callApi(url);
+        const addr = await dataList.map((item) => {
+          return { title: item.title, address: item.basicAddr };
+        });
+        setAddrData(addr);
+      } catch (error) {
+        setError(true);
+      }
+    }
+    fetchData();
+  }, []);
+  console.log(addrData);
   return <div> </div>;
 }
 
