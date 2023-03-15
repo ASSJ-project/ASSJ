31a87ce6d0efbae606b0a39d7a19c1869de39418
jaebyeoklee <jaebyeoklee@gmail.com>
2023-03-15 17:55:28 +0900

    fix:마커, 인포윈도우 선언부 위치 교체

--- frontend/src/components/Map/MapData.js
+++ frontend/src/components/Map/MapData.js
@@ -1,27 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import KakaoMap from "./KakaoMap";
 
 export default function MapData({ addrdata }) {
-  const [visible, setVisible] = useState(true);
-  const [mapSize, setMapSize] = useState([100, 80]);
+  const mapSize = [100, 80];
 
   // 차트찍기
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setMapSize([100, 80]);
-        }}
-      >
-        주변 회사 불러오기
-      </button>
       <div id="wrap">
-        {visible && (
-          <>
-            <h2>Kakao Map</h2>
-            <KakaoMap size={mapSize} data={addrdata} />
-          </>
-        )}
+        <KakaoMap size={mapSize} data={addrdata} />
       </div>
     </div>
   );
