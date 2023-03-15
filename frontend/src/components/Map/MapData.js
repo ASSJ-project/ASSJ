82afaf29b6eb47a2d423b19360e4bd3550367348
jaebyeoklee <jaebyeoklee@gmail.com>
2023-03-15 16:23:50 +0900

    fix:데이터 필터 region 기반으로 바꿈, mapdata.js 파일 정리

--- frontend/src/components/Map/MapData.js
+++ frontend/src/components/Map/MapData.js
@@ -1,62 +1,8 @@
 import React, { useState, useEffect } from "react";
 import KakaoMap from "./KakaoMap";
-import { callApi } from "../../functions";
 
 export default function MapData({ addrdata }) {
   const [visible, setVisible] = useState(true);
-  // const [markerPositions, setMarkerPositions] = useState([]);
-  // const [axis, setAxis] = useState([]);
-  // const [company, setCompany] = useState([]);
-
-  // useEffect(() => {
-  //   // 주소로 좌표 변환
-
-  //   async function getData() {
-  //     let url;
-  //     let data;
-  //     let x, y;
-  //     let axisList = [];
-  //     const header = {
-  //       Authorization: "KakaoAK 50bbb5205dc8fcc9c2611542015a54d5",
-  //     };
-
-  //     try {
-  //       //console.log(addrdata);
-  //       // for (let addr in addrdata) {
-  //       //   url =
-  //       //     "https://dapi.kakao.com/v2/local/search/address.json?&query=" +
-  //       //     addr["address"];
-  //       //   data = await callApi(url, header);
-  //       //   x = parseFloat(await data.documents[0].x);
-  //       //   y = parseFloat(await data.documents[0].y);
-  //       //   axisList.push(y, x);
-  //       //   setAxis((axis) => [...axis, axisList]);
-  //       //   setCompany((comp) => [...comp, addr.company]);
-  //       //   // console.log(data);
-  //       // }
-  //       // let dataList = [data, ...dataList];
-  //       // addrdata.forEach(async (add) => {
-  //       //   console.log(add);
-  //       //   url =
-  //       //     "https://dapi.kakao.com/v2/local/search/address.json?&query=" +
-  //       //     add["address"];
-  //       //   const data = await callApi(url, header);
-  //       //   // const x = parseFloat(await data.documents[0].x);
-  //       //   // const y = parseFloat(await data.documents[0].y);
-  //       //   // //console.log(addrdata);
-  //       //   // axisList.push(y, x);
-  //       //   // setAxis(async (axis) => [...axis, axisList]);
-  //       //   // setCompany(async (comp) => [...comp, add.company]);
-  //       //   // //console.log(axisList);
-  //       // });
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   }
-
-  //   getData();
-  // }, [addrdata]);
-
   const [mapSize, setMapSize] = useState([100, 80]);
 
   // 차트찍기
@@ -65,8 +11,6 @@ export default function MapData({ addrdata }) {
       <button
         onClick={() => {
           setMapSize([100, 80]);
-          // setCompany((comp) => [...comp, addrdata[0].company]);
-          // setMarkerPositions((poisit) => [poisit, ...addrdata[0].address]);
         }}
       >
         주변 회사 불러오기
