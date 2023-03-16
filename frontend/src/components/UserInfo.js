6e4cee51df87db6f1950886ed2c5d29e3aa45538
honghabi <sun742@naver.com>
2023-03-16 18:13:29 +0900

    fix:admin page세부메뉴 버튼 클릭시 페이지 이동,css변경, feat:userInfo component생성

---
+++ frontend/src/components/UserInfo.js
@@ -0,0 +1,24 @@
+import "../static/css/UserInfo.css";
+
+function UserInfo() {
+  return (
+    <div className="user_info_container">
+      <div className="box">ㅇㅅㅇ</div>
+      <div className="box">
+        <ul>
+          <li>이름</li>
+          <li>메일주소</li>
+          <li>주소</li>
+        </ul>
+      </div>
+      <div className="box">
+        <ul>
+          <li>dd</li>
+          <li>dd</li>
+          <li>dd</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default UserInfo;
