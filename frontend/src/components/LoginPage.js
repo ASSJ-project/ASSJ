ea9048c055a743f1e87f33ecab55d570f5109558
subway1112 <subway1112@naver.com>
2023-03-17 15:38:22 +0900

    fear: 로그인 페이지 css 수정, input박스 props 불러오기 구현

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,45 +1,41 @@
 import "../static/css/LoginPage.css";
 import React from "react";
-import GoogleLoginBtn from "./Login/GoogleLoginBtn";
-import KakaoLoginBtn from "./Login/KakaoLoginBtn";
-import NaverLoginBtn from "./Login/NaverLoginBtn";
+import GoogleLoginBtn from "./GoogleLoginBtn";
+import KakaoLoginBtn from "./KakaoLoginBtn";
+import NaverLoginBtn from "./NaverLoginBtn";
+import MainLogo from "../static/images/nuki02.png";
+import InputProps from "./input";
+
+//<p className="logintext">Login</p>
+//<input className="id-input" placeholder="Email Address" type="email" />
+//<input className="pw-input" placeholder="Password" type="password" />
 
 function LoginPage() {
   return (
     <>
-      <div className="container">
-        <p className="logintext">Login</p>
+      <div className="container">        
+        <img className="loginlogo" src={MainLogo} />
         <div className="id-container">
           <p className="id-text">Email</p>
-          <input
-            className="id-input"
-            placeholder="Email Address"
-            type="email"
-          />
-        </div>
-        <div className="errorMessage">
-          <div>Please verify your email</div>
+          <InputProps placeholder="Email" type="email" />
+          <div className="errorMessage">
+            Please verify your email
+          </div>
         </div>
 
         <div className="pw-container">
           <p className="pw-text">Password</p>
-          <input className="pw-input" placeholder="Password" type="password" />
+          <InputProps placeholder="Password" type="password" />
+          <div className="errorMessage">
+            Please verify your email
+          </div>
         </div>
-        <div className="errorMessage">
-          <div>Please verify your email</div>
+        <div className="find-pw-container">
+          <p className="find-pw"><a>forgot password?</a></p>
         </div>
-
-        <div className="onoff-switch-container">
-          <input type="checkbox" name="onoff-switch" id="onoff-switch1" />
-          <label htmlFor="onoff-switch1"></label>
-          <span className="toggle-text">Remember me</span>
-          <a className="find-pw">forgot password?</a>
-        </div>
-
-        <div>
+        <div className="login-container">
           <button className="login-btn">Login</button>
         </div>
-
         <div className="api-btn">
           <GoogleLoginBtn />
           <KakaoLoginBtn />
@@ -49,8 +45,7 @@ function LoginPage() {
         <div className="signup-div">
           <p>
             Don't have an account?{" "}
-            <span>
-              <a className="signup-btn"> Sign up</a>
+            <span className="signup-btn">
             </span>
           </p>
         </div>
