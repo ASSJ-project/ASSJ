2c4ca3229f6dad69096b8a30f99d57003148ee66
03enje <03enje@gmail.com>
2023-03-20 15:20:00 +0900

    update: 메인 폴더에서 company 도메인 분리

---
+++ backend/assj/src/main/java/com/assj/domain/company/CompanyController.java
@@ -0,0 +1,34 @@
+package com.assj.domain.company;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.assj.Dao;
+
+@RestController
+public class CompanyController {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @GetMapping("/api/getCorpData")
+    public List<Company> getAllCompanies() {
+        return companyService.getAllCompanies();
+    }
+
+    // 주소 좌표 호출
+    @GetMapping("/api/getGeo/{address}")
+    public List<Double> getGeo(@PathVariable String address) {
+        List<Double> obj = getGeo(address);
+        return obj;
+    }
+
+    @GetMapping("/api/setcorp")
+    public void test1() throws Exception {
+        companyService.initializeCompanyData();
+    }
+}
