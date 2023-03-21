09a2aaa2e0fa9db316da65a32c4bfeaa70eaa9d7
ahso0 <ahso0@naver.com>
2023-03-21 18:21:36 +0900

    fix : css 수정 및 컴포넌트 분리

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App";
 import App from "./main";
+import MyPage from "./pages/MyPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<MyPage />);
