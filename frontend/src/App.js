dd3078a27805366556cc55ff94a1ee53ac51a21d
03enje <03enje@gmail.com>
2023-03-21 13:02:31 +0900

    fix: import normalize.css 제거

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 // import MapData from "./components/Map/MapData";
 import React from "react";
 import LoginPage from "./components/LoginPage";
-import 'normalize.css';
 // import Test from "./Test";
 
 function App() {
