834dc634f7dd90ac9f9d628b762c1054c50caf56
03enje <03enje@gmail.com>
2023-03-24 12:32:29 +0900

    fix: 리액트 라우터 경로 수정 및 파일 병합

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,55 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./main";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'normalize.css'; // import normalize.css
+import MapPage from "./pages/MapPage";
+import LoginPage from "./pages/LoginPage";
+import StartPage from "./pages/StartPage";
+import RegisterPage from "./pages/RegisterPage";
+import Test from "./test/Test";
+import MyPage from "./pages/MyPage";
+import ErrorPage from "./pages/ErrorPage";
+import SideBar from "./pages/SideBar";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <StartPage />,
+  },
+  {
+    path: "/map",
+    element: <MapPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/test",
+    element: <Test />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/mypage",
+    element: <MyPage/>
+  },
+  {
+    path: "/*",
+    element : <ErrorPage/>
+  },
+  {
+    path : "/sidebar",
+    element : <SideBar/>
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+export default router;
