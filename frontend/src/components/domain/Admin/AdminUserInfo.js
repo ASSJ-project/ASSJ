4de09c45041f78f1b4b13d94b5fff713a86fd4d0
honghabi <sun742@naver.com>
2023-03-29 16:22:48 +0900

    회원정보 테이블 작성ing

--- frontend/src/components/domain/Admin/AdminUserInfo.js
+++ frontend/src/components/domain/Admin/AdminUserInfo.js
@@ -1,18 +1,34 @@
 // coding by 'ikki'
 import React from "react";
 import "./Admin.css";
-import UserInfo from "./UserInfo";
+import UserInfo from "./InfoTable";
 
 function AdminUserInfo() {
-  return (
-    <>
-      <div className="info_container">
-        <UserInfo />
-        <UserInfo />
-        <UserInfo />
-      </div>
-    </>
+  const columns = useMemo(
+    () => [
+      {
+        accessor: "id",
+        Header: "ID",
+      },
+      {
+        accessor: "name",
+        Header: "이름",
+      },
+      {
+        accessor: "email",
+        Header: "메일",
+      },
+      {
+        accessor: "adress",
+        Header: "주소",
+      },
+    ],
+    []
   );
+
+  const data = useMemo(() => []);
+
+  return <InfoTable columns={columns} data={data} />;
 }
 
 export default AdminUserInfo;
--- frontend/src/components/domain/Admin/UserInfo.js
+++
@@ -1,8 +0,0 @@
-// coding by 'ikki'
-import { registerDo } from "@/apis/register/registerDo";
-import "./UserInfo.css";
-
-function UserInfo() {
-  return <div className="user_info_container"></div>;
-}
-export default UserInfo;
