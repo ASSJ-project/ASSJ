788892e317ae3a8405a819a6a60e4448ee362a86
ahso0 <ahso0@naver.com>
2023-03-29 18:25:07 +0900

    feat:마이페이지,헤더 css 수정

--- frontend/src/components/domain/MyPage/MyPage-Header.js
+++ frontend/src/components/domain/MyPage/MyPage-Header.js
@@ -1,28 +1,37 @@
-import { useState } from "react";
-import "@/components/domain/MyPage/MyPage.css"
+import { useState } from 'react';
+import '@/components/domain/MyPage/MyPage.css';
 
-export default function MyPageHeader(props){
+export default function MyPageHeader(props) {
+  const [tab, setTab] = useState(false);
 
-  const[tab, setTab] = useState(false)
+  const mypage1 = () => {
+    props.getMyPage1(false);
+    setTab(false);
+  };
 
-    const mypage1 = () => {
-      props.getMyPage1(false);
-      setTab(false);
-    }
+  const mypage2 = () => {
+    props.getMyPage2(true);
+    setTab(true);
+  };
 
-    const mypage2 = () =>{
-      props.getMyPage2(true);
-      setTab(true);
-    }
-
-  
-    return(
-        <>    
-      <div className='mypage-mypage'>MY PAGE</div>
+  return (
+    <>
+      <div className="mypage-mypage">MY PAGE</div>
       <div className="mypage-clickmenu">
-        <button className={tab ? "mypage-myinfo2" : "mypage-myinfo1"} onClick={mypage1}>내정보</button>
-        <button className={tab ? "mypage-myinfo1" : "mypage-myinfo2"} style={{ textDecoration: "none" }} onClick={mypage2}>최근 조회 기록</button>
+        <button
+          className={tab ? 'mypage-myinfo2' : 'mypage-myinfo1'}
+          onClick={mypage1}
+        >
+          나의 정보
+        </button>
+        <button
+          className={tab ? 'mypage-myinfo1' : 'mypage-myinfo2'}
+          style={{ textDecoration: 'none' }}
+          onClick={mypage2}
+        >
+          최근 조회 기록
+        </button>
       </div>
-      </>
-    );
-};
+    </>
+  );
+}
