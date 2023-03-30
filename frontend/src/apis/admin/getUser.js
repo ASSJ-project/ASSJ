2d50ab6377d1dcc16cf85387ddaa579c514486ee
honghabi <sun742@naver.com>
2023-03-30 17:43:16 +0900

    admin 회원정보 db연동->table출력, 검색기능 구현

--- frontend/src/apis/admin/getUser.js
+++ frontend/src/apis/admin/getUser.js
@@ -4,8 +4,8 @@ export async function getUser() {
   const url = "api/users/all";
   try {
     const result = await axios.get(url);
-    console.log(result);
-    return result;
+
+    return result.data;
   } catch (error) {
     console.log(error);
   }
