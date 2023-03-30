fe812de8324790ca268609c5283ad5ca2585d1b8
03enje <03enje@gmail.com>
2023-03-30 12:57:45 +0900

    feat: useFetch 커스텀 훅 추가

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,3 +1,27 @@
-function useFetch() {
-  const { isLoading, error, data} = useQuery('data', fetchData);
+import { useState, useEffect } from 'react';
+
+function useFetch(url, headers, queryParams) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        // URL, 헤더, 쿼리 파라미터 등을 동적으로 설정
+        const response = await fetch(url, { headers, queryParams });
+        const json = await response.json();
+        setData(json);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url, headers, queryParams]);
+
+  return { data, isLoading, error };
 }
