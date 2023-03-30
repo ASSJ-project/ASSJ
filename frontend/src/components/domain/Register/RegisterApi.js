e7197ff1df5a617c9306a467b64e313c89d5b5eb
jaebyeoklee <jaebyeoklee@gmail.com>
2023-03-30 11:26:02 +0900

    fix:회원가입 페이지 state set 함수 통일, 코드최적화

--- frontend/src/components/domain/Register/RegisterApi.js
+++ frontend/src/components/domain/Register/RegisterApi.js
@@ -1,8 +1,10 @@
 /* global daum */
-export function postalSeach() {
+// 주소 검색 API
+export function postalSeach(setAddress) {
   new daum.Postcode({
     oncomplete: function (data) {
       document.getElementById("address").value = data.address;
+      setAddress(data.address);
     },
   }).open();
 }
