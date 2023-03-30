4d5fd23bc12bb9b1cc666699c7f7132f6da5f70d
honghabi <sun742@naver.com>
2023-03-30 17:53:14 +0900

    admin 회원정보 db연동->table출력, 검색기능 구현

--- frontend/src/components/domain/Admin/InfoTable.js
+++ frontend/src/components/domain/Admin/InfoTable.js
@@ -1,67 +1,26 @@
 // coding by 'ikki'
-import { getUser } from "@/apis/admin/getUser";
-import { useEffect, useState } from "react";
-import "./Admin.css";
+import {} from "@/apis/admin/getUser";
+import "./UserInfo.css";
 
-import Checkbox from "@mui/material/Checkbox";
-import Button from "@mui/material/Button";
-
-function InfoTable() {
-  const [userData, setUserData] = useState([]);
-  const [searchData, setSearchData] = useState([]);
-  const [searchText, setSearchText] = useState("");
-  useEffect(() => {
-    const getUserData = async () => {
-      const data = await getUser();
-      setUserData(data);
-      setSearchData(data);
-    };
-    getUserData();
-  }, []);
-
-  const filteredData = (data) => {
-    return data.filter((i) => i.userName.includes(searchText));
-  };
-
+function InfoTable({ columns, data }) {
   return (
     <div className="user_info_container">
-      <div className="search_input_btn">
-        <input
-          className="search_input"
-          type="text"
-          onChange={(e) => setSearchText(e.target.value)}
-        ></input>{" "}
-        <Button
-          className="search_btn"
-          onClick={() => setSearchData(filteredData(userData))}
-        >
-          검색
-        </Button>
-        <Button className="user_delete_btn" variant="contained">
-          삭제
-        </Button>
-      </div>
-      <table className="user_table">
+      <table>
         <thead>
           {/* head : id, name, email, adress */}
-          <tr className="head_title">
-            <th>ID</th>
-            <th>이름</th>
-            <th>메일</th>
-            <th>주소</th>
-            <th>선택</th>
+          <tr>
+            {columns.map((column) => (
+              <th key={column}>{column}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {searchData.map(({ uuid, userName, userEmail, userAddress }) => (
-            <tr key={uuid}>
-              <td>{uuid}</td>
-              <td>{userName}</td>
-              <td>{userEmail}</td>
-              <td>{userAddress}</td>
-              <td>
-                <Checkbox />
-              </td>
+          {data.map(({ id, name, email, adress }) => (
+            <tr key={id + name + email + adress}>
+              <td>{id}</td>
+              <td>{name}</td>
+              <td>{email}</td>
+              <td>{adress}</td>
             </tr>
           ))}
         </tbody>
---
+++ frontend/src/components/domain/Admin/UserInfo.js
@@ -0,0 +1,34 @@
+// coding by 'ikki'
+// import React from "react";
+// import "./Admin.css";
+// import UserInfo from "./InfoTable";
+
+// function AdminUserInfo() {
+//   const columns = useMemo(
+//     () => [
+//       {
+//         accessor: "id",
+//         Header: "ID",
+//       },
+//       {
+//         accessor: "name",
+//         Header: "이름",
+//       },
+//       {
+//         accessor: "email",
+//         Header: "메일",
+//       },
+//       {
+//         accessor: "adress",
+//         Header: "주소",
+//       },
+//     ],
+//     []
+//   );
+
+//   const data = useMemo(() => []);
+
+//   return <InfoTable columns={columns} data={data} />;
+// }
+
+// export default AdminUserInfo;
