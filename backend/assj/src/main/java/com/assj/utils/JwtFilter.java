4e96c5eb9dfabd396dd8fa751edeaa865b880873
03enje <03enje@gmail.com>
2023-04-03 18:30:03 +0900

    fix: 병합 충돌 해결

--- backend/assj/src/main/java/com/assj/utils/JwtFilter.java
+++ backend/assj/src/main/java/com/assj/utils/JwtFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,55 +18,64 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.assj.domain.user.UserService;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 
 import lombok.extern.slf4j.Slf4j;
 
 // OncePerRequestFilter 는 어떤 서블릿 컨테이너에서나 요청 당 한번의 실행을 보장하는 필터
 @Slf4j
-@Component
-public class JwtFilter extends OncePerRequestFilter {
+@Component 
+public class JwtFilter extends OncePerRequestFilter{
 
   @Value("${jwt.secret-key}")
   private String secretKey;
 
   @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-      throws IOException, ServletException {
-
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException{
+    
     // 리퀘스트 헤더에서 전달된 토큰을 가져옴
     final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
 
     // 해당 토큰이 비어있거나 Bearer로 시작되지 않으면 리턴
-    if (authorization == null || !authorization.startsWith("Bearer ")) {
+    if(authorization == null|| !authorization.startsWith("Bearer ")){
       filterChain.doFilter(request, response);
       return;
     }
 
     // Bearer를 제외한 뒤쪽의 토큰 정보만을 잘라냄
     String token = authorization.split(" ")[1];
+    String refresh = authorization.split(" ")[2];
+ 
+    
 
-    // token expired 여부 확인
-    try {
+    // token expired 여부 확인 
+    try{
       JwtToken.isExpired(token, secretKey);
-    } catch (TokenExpiredException e) {
+    }catch(TokenExpiredException e){
       log.error("토큰이 만료되었습니다");
+
+      // 리프레시 토큰을 이용해 엑세스 토큰 자동 발급 
+
+      //response.setHeader("access denied", "token is expired");
+      
+
       filterChain.doFilter(request, response);
       return;
     }
 
     // token 에서 꺼낸 user email
     String userEmail = JwtToken.getUserEmail(token, secretKey);
+    
     // token 에서 꺼낸 user role 쌍따옴표가 붙어서 들어오기 때문에 제거
-    String role = JwtToken.getUserRole(token, secretKey).replaceAll("\\\"", "");
+    String role = JwtToken.getUserRole(token, secretKey).replaceAll("\\\"","");
 
-    // 권한 부여
-    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userEmail, null,
-        List.of(new SimpleGrantedAuthority(role)));
+    // 권한 부여 
+    UsernamePasswordAuthenticationToken authenticationToken = 
+      new UsernamePasswordAuthenticationToken(userEmail, null, List.of(new SimpleGrantedAuthority(role)));
 
-    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-    System.out.println(authenticationToken.getAuthorities());
-    filterChain.doFilter(request, response);
-  }
-}
+      authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+      SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+      System.out.println(authenticationToken.getAuthorities());
+      filterChain.doFilter(request, response);
+  }}
