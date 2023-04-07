67deedc8e8f46629421d38b090a52409cfa4e0f6
ahso0 <ahso0@naver.com>
2023-04-07 16:02:57 +0900

    feat:마이페이지 role에 따라 나의정보 나오는 거 수정

--- frontend/src/pages/ErrorPage.js
+++ frontend/src/pages/ErrorPage.js
@@ -1,15 +1,16 @@
-import "@/components/domain/Error/ErrorPage.css";
-import logo2 from "../assets/images/Error-logo.png";
-import { Link } from "react-router-dom";
-import Header from "@/components/Structure/Header/Header";
-import Button from "@mui/material/Button";
-import Footer from "@/components/Structure/Footer/Footer";
-import { useState, useEffect } from "react";
+import '@/components/domain/Error/ErrorPage.css';
+import logo2 from '../assets/images/Error-logo.png';
+import { Link } from 'react-router-dom';
+import Header from '@/components/Structure/Header/Header';
+import Button from '@mui/material/Button';
+import Footer from '@/components/Structure/Footer/Footer';
+import { useState, useEffect } from 'react';
 
 const ErrorPage = () => {
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
+
   useEffect(() => {
-    setError(sessionStorage.getItem("error_message"));
+    setError(sessionStorage.getItem('error_message'));
   }, [error]);
   return (
     <>
@@ -19,12 +20,11 @@ const ErrorPage = () => {
           <img src={logo2} alt="Error-logo2" className="Error-logo2" />
         </div>
         <div className="Error-text">
-          <h3>{error}</h3>
+          {error ? <h2>{error}</h2> : <h2>PAGE NOT FOUND</h2>}
         </div>
-
-        <Link to="/" style={{ textDecoration: "None" }}>
+        <Link to="/" style={{ textDecoration: 'None' }}>
           <Button className="Error-home" variant="contained">
-            Start Over
+            HOME
           </Button>
         </Link>
       </div>
